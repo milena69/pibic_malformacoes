d8fdec1c58d57296d955b4fd082e50558d1eac87
milena0202 <milanike69@gmail.com>
2022-01-12 22:11:23 -0300

    fix: ajuste no styled components

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,18 +1,19 @@
 import ArrowCircleRightIcon from "@mui/icons-material/ArrowCircleRight";
 import { Botao } from "./styled";
 
-export const Buttons = ({ icone, isLoading }) => {
+export const Buttons = ({ icone, isLoading, titulo, voltar }) => {
   return (
     <div>
       <Botao
+        voltar={voltar}
         loading={isLoading}
         loadingPosition="end"
-        
+
         // color="primary"
         //endIcon={<ArrowCircleRightIcon />} //{icone}
         // variant="contained"
       >
-        Próximo
+        {titulo}
       </Botao>
     </div>
   );
--- src/components/container/styled.js
+++ src/components/container/styled.js
@@ -2,24 +2,15 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/css";
 import Container from "@mui/material/Container";
 
-export const Containers = styled(Container, {
-  color: "text.secondary",
-  m: 2,
-  p: 2,
-  sx: {
-    my: 2,
-  },
-})`
+export const Containers = styled(Container)`
   box-shadow: 0px 3px 10px #d6d6d6, 0px 0px 33px #d6d6d6, 0px 8px 400px #d6d6d6;
-
   height: 100%;
   border-radius: 1rem;
   width: 50%;
   display: flex;
   align-items: center;
   margin: 15% auto;
-  justify-content: center;
-  /* padding: 50px 0px; */
+  padding: 50px 0px;
 
   /* background-color: ${({ theme: { colors } }) => colors.background}; */
 `;
